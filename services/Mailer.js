5e469a83983e8eaadd16e3295985c48739437399
Anmeet <anmeet619@yahoo.com>
2020-06-28 11:24:23 +1000

    final commit

--- services/Mailer.js
+++ services/Mailer.js
@@ -7,7 +7,7 @@ class Mailer extends helper.Mail {
     super();
 
     this.sgApi = sendgrid(keys.sendGridKey);
-    this.from_email = new helper.Email("nitikapokharel@gmail.com");
+    this.from_email = new helper.Email("anmeet916@gmail.com");
     this.subject = subject;
     this.body = new helper.Content("text/html", content);
     this.recipients = this.formatAddresses(recipients);
